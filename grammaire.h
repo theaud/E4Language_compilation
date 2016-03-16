f317a49ba839fd1fd30577b170d6222d81b7e1e8
theaud <brionmathieu@gmail.com>
2016-03-16 09:54:49 +0100

    3 commit

---
+++ grammaire.h
@@ -0,0 +1,21 @@
+
+#ifndef GRAMMAIRE
+#define GRAMMAIRE
+#include <string>
+#include "../liste.h"
+
+class Grammaire{
+private :
+	string nom;
+	Liste<string> valeur;
+public :
+	Grammaire(){}
+	Grammaire(char *str);
+	~Grammaire(){}
+	friend ostream& operator<<(ostream &os, const Grammaire &grammaire);
+	static Liste<Grammaire> getgrammaires();
+	const string& getnom()const;
+	const Liste<string>& getvaleur()const;
+};
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
-
 using namespace std;
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../fonctions.h"
+#include "grammaire.h"
 
-int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
-}
+int main(int argc, char *argv[]){
+	Liste<Grammaire> grammaires = Grammaire::getgrammaires();
+	cout << grammaires << endl;
+	system("PAUSE");}
